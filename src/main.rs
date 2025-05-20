a4f4c5af96f57754f4264e2ecececaec9302953b
Leo <leo.morat@epitech.eu>
2025-05-20 15:56:29 +0200

    [ADD] driver

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use soapysdr::Direction::{Rx, Tx};
 use num_complex::Complex;
 
-fn main() {
-    let channel: usize = 0;
-    let mut num: usize = 0;
-    let mut freq =  433690000.0;
+fn init_driver_sdr(channel: usize, mut num: usize, mut freq: f64) -> soapysdr::Device {
     for dev in soapysdr::enumerate("").unwrap() {
         println!("{}", dev);
         let dev = soapysdr::Device::new(dev).expect("Error opening device");
@@ -32,25 +29,20 @@ fn main() {
             }
         }
         dev.set_sample_rate(Rx, channel, 2_400_000.0).expect("Failed to set sample rate");
-
         let mut stream = dev.rx_stream::<Complex<f32>>(&[channel]).unwrap();
         let mut buf = vec![Complex::new(0.0, 0.0); stream.mtu().unwrap()];
-
-
         stream.activate(None).expect("failed to activate stream");
-        for i in 0..20{
-            dev.set_frequency(soapysdr::Direction::Rx, channel,  freq, ()).expect("Failed to set frequency");
-            for i in 0..10{
-                let read_size =buf.len();
-                let len = stream.read(&mut [&mut buf[..read_size]], 1_000_000).expect("read failed");
-                let power: f32 = buf.iter().map(|s| s.re * s.re + s.im * s.im).sum::<f32>() / buf.len() as f32;
-                println!("Average power: {}", power);
-            }
-        }
-        for data in buf {
-            let magnitude = (data.re * data.re + data.im * data.im).sqrt();
-            println!("{:X}", (magnitude * 10000.0) as u32);
-        }
-        stream.deactivate(None).expect("failed to deactivate");
+        dev.set_frequency(soapysdr::Direction::Rx, channel,  freq, ()).expect("Failed to set frequency");
+        //stream.deactivate(None).expect("failed to deactivate");
+    }
+    return dev;
+}
+
+fn main() {
+    let channel: usize = 0;
+    let num: usize = 0;
+    let freq =  433690000.0;
+    match init_driver_sdr(channel, num, freq) {
+
     }
 }
